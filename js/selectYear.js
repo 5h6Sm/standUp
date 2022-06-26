e9458ce11eda55ae18e92a75e86b1100179f8856
수민 <5h6sm.github@gmail.com>
2022-06-26 21:53:34 +0900

    selectYear

---
+++ js/selectYear.js
@@ -0,0 +1,9 @@
+let a = document.querySelector('.touch');
+let b = document.querySelector('.select-ul')
+let c = document.querySelector('.select-option')
+
+
+a.addEventListener('click', function() {
+    b.classList.toggle('show-option');
+  
+});
